619080086f72b7c1366d59140e18d7ac8ec285f0
Dnyaneshwar Ghodake <dnyaneshghodake@gmail.com>
2026-03-04 14:49:19 +0530

    Initial commit

---
+++ src/main/java/com/ledgora/LedgoraApplication.java
@@ -0,0 +1,13 @@
+package com.ledgora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LedgoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LedgoraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ledgora/LedgoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ledgora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LedgoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
